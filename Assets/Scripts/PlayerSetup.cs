df8bca9eac07293de2cfb823270de887d609152e
SophosXXX <arjunreddymali@yahoo.com>
2024-04-25 22:00:47 -0500

    Voice chat

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class PlayerSetup : MonoBehaviour
+public class PlayerSetup : MonoBehaviourPunCallbacks
 {
-    // public CharacterMovement movement;
     public GameObject camera;
-    public void isLocalPlayer(){
-        // movement.enabled = true;
+    public GameObject character; // Reference to the character GameObject
+
+    [PunRPC]
+    public void isLocalPlayer()
+    {
         camera.SetActive(true);
 
+        // Change character color to a random color
+        Renderer renderer = character.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material.color = Random.ColorHSV();
+        }
+        else
+        {
+            Debug.LogError("Character GameObject does not have a Renderer component.");
+        }
     }
 }
--- Assets/Scripts/StartGameButton.cs
+++ Assets/Scripts/StartGameButton.cs
@@ -5,6 +5,6 @@ public class StartGameButton : MonoBehaviour
 {
     public void LoadGameScene()
     {
-        SceneManager.LoadScene("./Project");
+        SceneManager.LoadScene("Project");
     }
 }
